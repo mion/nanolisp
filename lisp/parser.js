b744d24db1f31ced71165474862da474b4822d11
Gabriel Vieira <gluisvieira@gmail.com>
2014-04-13 00:08:37 -0300

    atoms

--- lisp/parser.js
+++ lisp/parser.js
@@ -11,6 +11,24 @@ var tokenize = function (str) {
             });
 };
 
+var atom = function (token) {
+  if (token === "true") {
+    return bool(true);
+  }
+  if (token === "false") {
+    return bool(false);
+  }
+  if (!_.isNaN(parseInt(token))) {
+    return number(parseInt(token));
+  }
+  if ((token.length >= 2) && (token[0] === '"') && (token[token.length - 1] === '"')) {
+    return string(token.slice(1, token.length - 1));
+  }
+
+  // TODO: things like "-" can become symbols
+  return symbol(token);
+};
+
 var parse = function (str) {
   return error();
 };
--- tests/parser.js
+++ tests/parser.js
@@ -8,6 +8,22 @@ test( "tokens", function () {
 });
 
 test( "atoms", function () {
+  deepEqual( atom("0"), number(0) );
+  deepEqual( atom("-1"), number(-1) );
+  deepEqual( atom("+429"), number(429) );
+
+  deepEqual( atom("foo"), symbol("foo") );
+
+  deepEqual( atom("\"foo\""), string("foo") );
+  deepEqual( atom("\"hello world\""), string("hello world") );
+  deepEqual( atom("\"f\""), string("f") );
+  deepEqual( atom("\"\""), string("") );
+
+  deepEqual( atom("true"), bool(true) );
+  deepEqual( atom("false"), bool(false) );
+});
+
+test( "parse", function () {
   deepEqual( parse("1"), number(1) );
   deepEqual( parse("-42"), number(-42) );
 
