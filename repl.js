89eb8c98f8a64c4601dcc0103e2be677b0a4126b
Gabriel Vieira <gluisvieira@gmail.com>
2014-04-14 00:04:15 -0300

    adjusts

--- repl.js
+++ repl.js
@@ -6,11 +6,13 @@
 CodeMirror.commands.save = function () { alert('Saving...'); }
 
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+	value: "(def square (fn (x) (* x x)))",
 	lineNumbers: true,
 	mode: "clojure",
 	tabSize: 2,
+	vimMode: true,
 	matchBrackets: true,
-	theme: "mdn-like"
+	// theme: "mdn-like"
 });
 
 var output = CodeMirror.fromTextArea(document.getElementById("output"), {
@@ -18,3 +20,5 @@ var output = CodeMirror.fromTextArea(document.getElementById("output"), {
 	tabSize: 2,
 	readOnly: true
 });
+
+// output.setValue
