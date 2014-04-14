fd8d00cdec7f4c11fe421d77b85ef3da9e33fdde
Gabriel Vieira <gluisvieira@gmail.com>
2014-04-13 22:37:58 -0300

    shitload of gayass designer shit

---
+++ repl.js
@@ -0,0 +1,11 @@
+/*
+ * 	nanolisp v0.1
+ *	Author: mion
+ */
+
+CodeMirror.commands.save = function () { alert('Saving...'); }
+var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+	lineNumbers: true,
+	mode: "clojure",
+	tabSize: 2
+});
